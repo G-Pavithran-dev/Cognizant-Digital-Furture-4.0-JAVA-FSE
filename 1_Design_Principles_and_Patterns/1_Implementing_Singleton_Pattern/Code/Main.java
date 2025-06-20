da0e1babdadc40746fb30bef758250bbe8484181
G-Pavithran-dev <pavithranadithya1441@gmail.com>
2025-06-20 13:09:07 +0530

    Completed 1_Implementation_of_SingleTon_Pattern

--- 1_Design_Principles_and_Patterns/1_Implementing_Singleton_Pattern/Code/Main.java
+++ 1_Design_Principles_and_Patterns/1_Implementing_Singleton_Pattern/Code/Main.java
@@ -1,24 +1,31 @@
-class SingleTon {
-    private static SingleTon singleTonInstance = new SingleTon();
+class Logger {
+    private static Logger singleTonInstance = new Logger();
 
-    private SingleTon() {
-        System.out.println("SingleTon Class Created.");
+    private Logger() {
+        System.out.println("Logger Class Created.\n");
     }
 
-    public static SingleTon getInstance() {
+    public void log(String message) {
+        System.out.println("Info : " + message);
+    }
+
+    public static Logger getInstance() {
         return singleTonInstance;
     }
 }
 public class Main {
     public static void main(String[] args) {
-        SingleTon instance1 = SingleTon.getInstance();
-        SingleTon instance2 = SingleTon.getInstance();
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
 
-        if(instance1 == instance2) {
-            System.out.println("Both instances points to same memory.");
+        logger1.log("Log test from Logger 1");
+        logger2.log("Log test from Logger 2");
+
+        if(logger1 == logger2) {
+            System.out.println("\nBoth loggers points to same memory.");
         }
         else {
-            System.out.println("Both instance not points to same memory.");
+            System.out.println("Both logger not points to same memory.");
         }
     }
 }
