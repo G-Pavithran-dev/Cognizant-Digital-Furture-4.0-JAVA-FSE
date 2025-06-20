3a55c1e6df866f2b5f14e4d706eeef5afb950ea3
G-Pavithran-dev <pavithranadithya1441@gmail.com>
2025-06-20 12:31:02 +0530

    Completed 1_Implementation_SingleTon_Pattern

--- 1_Design_Principles_and_Patterns/1_Implementing_Singleton_Pattern/Code/Main.java
+++ 1_Design_Principles_and_Patterns/1_Implementing_Singleton_Pattern/Code/Main.java
@@ -1,1 +1,24 @@
+class SingleTon {
+    private static SingleTon singleTonInstance = new SingleTon();
 
+    private SingleTon() {
+        System.out.println("SingleTon Class Created.");
+    }
+
+    public static SingleTon getInstance() {
+        return singleTonInstance;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        SingleTon instance1 = SingleTon.getInstance();
+        SingleTon instance2 = SingleTon.getInstance();
+
+        if(instance1 == instance2) {
+            System.out.println("Both instances points to same memory.");
+        }
+        else {
+            System.out.println("Both instance not points to same memory.");
+        }
+    }
+}
